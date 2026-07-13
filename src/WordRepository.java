739589e2619d7c0f2090158e4d3bd38fb40095c2
Сергей Комышанка <skomyshanka@mail.ru>
2026-07-13 15:59:00 +0300

    Класс слов

--- src/WordRepository.java
+++ src/WordRepository.java
@@ -2,5 +2,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 public class WordRepository {
+    private static final List<String> WORDS = Arrays.asList(
+            "разработка", "дифференциал", "матрица", "интеграл", "автомат"
+    );
+
+    private final Random random = new Random();
+
+    public String getRandomWord() {
+        return WORDS.get(random.nextInt(WORDS.size()));
+    }
 
 }
